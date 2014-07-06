f8e45d34b11f2d21909a0f1ca558273d3dab9ffc
Benjamin Saunders <ben.e.saunders@gmail.com>
2014-07-06 11:48:04 -0700

    Usage message

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@
 
 int main(int argc, char **argv) {
   if(argc < 3) {
-    fprintf(stderr, "FAIL: Insufficient arguments\n");
+    fprintf(stderr, "Usage: %s <output file> <url>*\n", argv[0]);
     return 1;
   }
 
